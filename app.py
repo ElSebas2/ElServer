0688e4de3a4cd57ce16feff48e1c1697bf4a558a
Sebastian Andres Muñoz Muñoz <107899667+ElSebas2@users.noreply.github.com>
2023-12-05 21:16:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,15 @@ def handle_json():
     data = json.loads(request.data.decode('utf-8'))
     dataJSON = data['object']['name']
     print(f'Data recibida: {dataJSON}')
-    # Aquí puedes guardar 'data' en la variable que quieras
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
     return 'OK, JSON recibido'
 
-@app.route('/string', methods=['POST'])
 def handle_string():
     global dataSTRING
     dataSTRING = request.data.decode('utf-8')
     print(f'String recibido: {dataSTRING}')
-    # Aquí puedes guardar 'data' en la variable que quieras
+    with open('data.txt', 'w') as f:
+        f.write(dataSTRING)
     return 'OK, STRING recibido'
+
